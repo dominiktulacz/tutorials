4037db5b27fb36f4bf3ba306afe7a160a861bef9
Dominik Tułacz <dominiktulacz@gmail.com>
2020-02-11 20:57:09 +0100

    swift-ui: Creating and Combining Views

---
+++ swift-ui/Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Dominik Tułacz on 11/02/2020.
+//  Copyright © 2020 Dominik Tułacz. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
